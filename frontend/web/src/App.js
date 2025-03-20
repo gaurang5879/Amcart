71bd6f6d1f037e4d5c89c5d9032498424c5860d1
gaurangsinghal <gaurang.singhal@nagarro.com>
2025-03-20 23:56:01 +0530

    updated code

--- frontend/web/src/App.js
+++ frontend/web/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
